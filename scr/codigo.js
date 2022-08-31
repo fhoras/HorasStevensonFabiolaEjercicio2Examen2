d74cfed362a983f9c8ae86de269f74d1a69d3fab
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-08-31 01:31:19 -0300

    initial commit

--- scr/codigo.js
+++ scr/codigo.js
@@ -1,18 +1,18 @@
 let boton = document.querySelector("#botonEnvio");
+
 let cantidad = document.getElementById("cantidad");
 boton.addEventListener("click", () => {
-  cantidad = Number(cantidad.value);
+  let cantidadValue = Number(cantidad.value);
   //verificarNulos(cantidad);
-  console.log(cantidad);
+  console.log(cantidadValue);
   // Agregar tantos botones como indique la cantidad
   //let divContenedor = document.querySelector("#");
 
-  for (let i = 1; i <= cantidad; i++) {
+  for (let i = 1; i <= cantidadValue; i++) {
     let miDiv = document.getElementById("divContenedor");
-    let boton = document.createElement("button");
-    boton.type = "button";
-    boton.innerHTML = "Boton Nuevo " + i;
-    boton.className = "botonAgregar";
-    miDiv.append(boton);
+    let div = document.createElement("div");
+    div.innerHTML = "Div Nuevo " + i;
+    div.className = "botonAgregar";
+    miDiv.append(div);
   }
 });
